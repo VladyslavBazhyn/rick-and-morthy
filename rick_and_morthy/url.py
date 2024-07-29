80dca4027425664697588ee8e302fea04ec1e439
Gibbs <mr.darmstadtiumgmail.com>
2024-07-29 16:56:52 +0200

    Added character list andpointwith filter

--- rick_and_morthy/url.py
+++ rick_and_morthy/url.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from rick_and_morthy.views import get_random_character
+from rick_and_morthy.views import get_random_character, CharacterListView
 
 app_name = "characters"
 
 urlpatterns = [
     path("characters/random", get_random_character, name="character-random"),
+    path("characters/", CharacterListView.as_view(), name="character-list")
 ]
