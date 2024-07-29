4a4c911dbb529fb811cac564569008019818c99f
Gibbs <mr.darmstadtiumgmail.com>
2024-07-29 15:26:13 +0200

    Added Character model and migrations

--- rick_and_morthy/admin.py
+++ rick_and_morthy/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from rick_and_morthy.models import Character
+
+
+admin.site.register(Character)
--- rick_and_morthy/models.py
+++ rick_and_morthy/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    class StatusChoices(models.TextChoices):
+        ALIVE = "Alive"
+        DEAD = "Dead"
+        UNKNOWN = "Unknown"
+
+    class GenreChoices(models.TextChoices):
+        FEMALE = "Female"
+        MALE = "Male"
+        GENDERLESS = "Genderless"
+        UNKNOWN = "Unknown"
+
+    api_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=30, choices=StatusChoices.choices)
+    species = models.CharField(max_length=255)
+    genre = models.CharField(max_length=30, choices=GenreChoices.choices)
+    image = models.URLField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
