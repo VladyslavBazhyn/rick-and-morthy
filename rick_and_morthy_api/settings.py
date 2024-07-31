fc5929f68bbf196ef2651bcacf3eaa39f22c95c7
Gibbs <mr.darmstadtiumgmail.com>
2024-07-31 07:58:34 +0200

    Added how to run tutorial. Switched to postgresql database

--- rick_and_morthy_api/settings.py
+++ rick_and_morthy_api/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = "rick_and_morthy_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "rick_and_morthy",
+        "USER": "rick_and_morthy_user",
+        "PASSWORD": "rick_and_morthy_password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
